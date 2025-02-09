1e3b7863f4468ced0c34d958973de5d319dfcf14
BlenderChen <ocsar93@gmail.com>
2025-02-08 20:42:49 -0800

    UI

--- Playbook/properties/team_properties.py
+++
@@ -1,7 +0,0 @@
-class TeamProperties:
-    id: str
-    name: str
-
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
--- Playbook/properties/workflow_properties.py
+++
@@ -1,9 +0,0 @@
-class WorkflowProperties:
-    id: str
-    team_id: str
-    name: str
-
-    def __init__(self, id, team_id, name):
-        self.id = id
-        self.team_id = team_id
-        self.name = name
